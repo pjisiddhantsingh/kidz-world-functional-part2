1f72b00fefa8bf8833df1ac7d9194f460cacb3e1
pjisiddhantsingh <159141612+pjisiddhantsingh@users.noreply.github.com>
2024-03-11 14:39:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,89 +1,89 @@
-var cartValue = document.getElementById("cart-value");
-var cartButton = document.getElementById("cart");
+var cartValue = document.getElementById('cart-value');
+var cartButton = document.getElementById('cart');
 
-var addButtons = document.getElementsByClassName("button");
+var addButtons = document.getElementsByClassName('button');
 
 var items = [
   {
-    name: "This was our pact",
+    name: 'This was our pact',
     quantity: 0,
     dollars: 7,
     cents: 49,
   },
   {
-    name: "The famous five",
+    name: 'The famous five',
     quantity: 0,
     dollars: 4,
     cents: 59,
   },
   {
-    name: "Matilda",
+    name: 'Matilda',
     quantity: 0,
     dollars: 6,
     cents: 80,
   },
   {
-    name: "Harry Potter",
+    name: 'Harry Potter',
     quantity: 0,
     dollars: 10,
     cents: 0,
   },
   {
-    name: "For Young Readers",
+    name: 'For Young Readers',
     quantity: 0,
     dollars: 7,
     cents: 29,
   },
   {
-    name: "Wimpy Kid - DIY",
+    name: 'Wimpy Kid - DIY',
     quantity: 0,
     dollars: 4,
     cents: 99,
   },
   {
-    name: "Dart Board",
+    name: 'Dart Board',
     quantity: 0,
     dollars: 17,
     cents: 49,
   },
   {
-    name: "Connect Four",
+    name: 'Connect Four',
     quantity: 0,
     dollars: 19,
     cents: 99,
   },
   {
-    name: "Jenga",
+    name: 'Jenga',
     quantity: 0,
     dollars: 20,
     cents: 99,
   },
   {
-    name: "Monopoly",
+    name: 'Monopoly',
     quantity: 0,
     dollars: 19,
     cents: 49,
   },
   {
-    name: "Bookmarks",
+    name: 'Bookmarks',
     quantity: 0,
     dollars: 12,
     cents: 49,
   },
   {
-    name: "Birthday Card",
+    name: 'Birthday Card',
     quantity: 0,
     dollars: 12,
     cents: 49,
   },
   {
-    name: "Stuffed toys",
+    name: 'Stuffed toys',
     quantity: 0,
     dollars: 15,
     cents: 99,
   },
   {
-    name: "Dream catcher drawing",
+    name: 'Dream catcher drawing',
     quantity: 0,
     dollars: 18,
     cents: 49,
@@ -107,6 +107,15 @@ for (let i = 0; i < addButtons.length; i++) {
 
 var finalDollars = 0;
 var finalCents = 0;
+function whatsappIntegration() {
+  for (let index = 0; index < items.length; index++) {
+    if (items[index].quantity != 0) {
+      apiLink += '%0A' + items[index].name + '%20' + items[index].quantity;
+    }
+  }
+  apiLink +=
+    '%0A' + 'Total%20Price:%20$' + finalDollars + '%20' + finalCents + 'c';
+}
 
 function updatePrice() {
   let totalPriceInCents = 0;
@@ -119,24 +128,26 @@ function updatePrice() {
   finalDollars = Math.floor(totalPriceInCents / 100);
   finalCents = totalPriceInCents % 100;
 }
-
+let apiLink =
+  'https://api.whatsapp.com/send?phone=919000000000&text=Order%20details';
 
 cartButton.onclick = () => {
   updatePrice();
-
+  whatsappIntegration();
+  window.open(apiLink, '_blank');
 
   for (let index = 0; index < items.length; index++) {
     if (items[index].quantity != 0) {
       console.log(
-        "Item name: " +
+        'Item name: ' +
           items[index].name +
-          " - Quantity: " +
+          ' - Quantity: ' +
           items[index].quantity
       );
     }
   }
 
   console.log(
-    "The total amount is " + finalDollars + "$ and " + finalCents + " cents"
+    'The total amount is ' + finalDollars + '$ and ' + finalCents + ' cents'
   );
 };
